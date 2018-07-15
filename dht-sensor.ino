152b6c65788bb12ca5055d306c447e40a24f5ec0
Kyle <ssjmetroid@gmail.com>
2018-07-15 22:26:52 +0000

    Learning to configure the bluetooth modem, still not sure if I can program this to use a reasonable amount of power...

--- dht-sensor.ino
+++ dht-sensor.ino
@@ -1,9 +1,13 @@
 #include <DHT.h>
 #include <SoftwareSerial.h>
+#define SENSOR_ID        0
 #define PIN_DHT          2
 #define PIN_BLUETOOTH_TX 4
 #define PIN_BLUETOOTH_RX 7
+#define ACK_STRING_LENGTH 3
 #define DHTTYPE DHT22
+const char ACK_STRING[ACK_STRING_LENGTH + 1] = "ACK";
+unsigned int currStringIndexAck = 0;
 DHT dht(PIN_DHT, DHTTYPE);
 SoftwareSerial bluetooth(PIN_BLUETOOTH_TX, PIN_BLUETOOTH_RX);
 unsigned long ioTime = 0;
@@ -19,16 +23,62 @@ int vccRead(byte us = 250)
   word x = ADC;
   return x ? (1100L*1023)/x : -1;
 }
+bool serialStringCompare(const char* const str, unsigned strSize,
+  char nextChar, unsigned int* currStrIndex)
+{
+  if(*currStrIndex < strSize &&
+    str[*currStrIndex] == nextChar)
+  {
+    (*currStrIndex)++;
+    if(*currStrIndex >= strSize)
+    {
+      *currStrIndex = 0;
+      return true;
+    }
+    // do not reset currStrIndex because we are still reading
+    //  a valid string match up to this point!
+    return false;
+  }
+  *currStrIndex = 0;
+  return false;
+}
 void setup()
 {
+  // MUST WAIT 500ms for the bluetooth modem to boot
+  //  see documentation page 45
+  // https://cdn.sparkfun.com/datasheets/Wireless/Bluetooth/bluetooth_cr_UG-v1.0r.pdf
+  // Assuming, ofc, that cpu & modem power on simultaneously
+  delay(500);
   Serial.begin(9600);
   Serial.println("DHT22 sensor");
   dht.begin();
+  // set the bluetooth serial line to the modem's default baud
   bluetooth.begin(115200);
-  bluetooth.print("$");
-  bluetooth.print("$");
-  bluetooth.print("$");
-  delay(100);
+  // enter command mode on the modem
+  bluetooth.print("$$$");
+  delay(100);// wait for "CMD" response
+  // Adjust the Inquiry Scan Window to the lowest possible value
+  //  (uses less power, delays discovery time)
+  bluetooth.println("SI,0100"); //default
+  //bluetooth.println("SI,0012");
+  delay(100);// wait for "AOK" response
+  // Adjust the Page Scan Window to the lowest possible value
+  //  (uses less power, delays connection time)
+  //bluetooth.println("SJ,0012");
+  bluetooth.println("SJ,0100"); //default
+  delay(100);// wait for "AOK" response
+  /* Apparently this doesn't actually work because MASTER has to agree...
+  // Enable Sniff Mode to put the radio to sleep every X milliseconds
+  bluetooth.println("SW,12C0");
+  delay(100);// wait for "AOK" response
+  */
+  // Lower the transmission power to lowest level
+  //  (uses less power, decreases transmission range)
+  bluetooth.println("SY,0010"); //default
+  //bluetooth.println("SY,FFF4");
+  delay(100);// wait for "AOK" response
+  // lower the baud of the modem as well as the serial interface
+  //  this command also takes the modem out of command mode
   bluetooth.println("U,9600,N");
   bluetooth.begin(9600);
 }
@@ -39,10 +89,21 @@ void loop()
   /// the unsigned long loops after ~50 days or so
   if (currTime - ioTime > 10000)
   {
+    /*
+    // enter data mode/reconnect to the bluetooth
+    //  -Leave quiet mode using "Q,0", wait a little bit for "AOK" response
+    bluetooth.println("Q,0");
+    delay(100);// wait for "AOK" response
+    // lower the baud of the modem as well as the serial interface
+    //  this command also takes the modem out of command mode
+    bluetooth.println("U,9600,N");
+    bluetooth.begin(9600);
+    // wait for the modem to reconnect
+    delay(3000);
+    */
     const float humidity    = dht.readHumidity();
     const float temperature = dht.readTemperature();
     const float voltage     = vccRead()/1000.f;
-    ///TODO: calculate voltage
     if (isnan(humidity)    || 
         isnan(temperature) || 
         isnan(voltage))
@@ -57,12 +118,40 @@ void loop()
       String(humidity   , 1) + "%,T=" +
       String(temperature, 1) + "C,V=" +
       String(voltage    , 3) + "U");
+    // wait some time for the data to be transmitted, hopefully
+    //  by the time this delay finishes our bluetooth serial
+    //  connection has an acknowledgement in the buffer.. ~
     delay(3000);
   }
   if (bluetooth.available())
   {
+    const char nextBtChar = (char)bluetooth.read();
+    /*
+    if(serialStringCompare(ACK_STRING, ACK_STRING_LENGTH,
+      nextBtChar, &currStringIndexAck))
+    {
+      // At this point, we have read the entire ack string
+      //  from the bluetooth connection, so we know that a
+      //  complete data string was sent over the connection //
+      //  -enter command mode on the bluetooth
+      // set the bluetooth serial line to the modem's default baud
+      bluetooth.begin(115200);
+      // enter command mode on the modem
+      bluetooth.print("$$$");
+      delay(100);// wait for "CMD" response
+      //  -kill the bluetooth connection (maybe not needed w/ quiet mode?)
+      //    command="K,", wait a little bit for the "KILL<cr><lf>" response
+      bluetooth.println("K,");
+      delay(100);// wait for "KILL<cr><lf>" response
+      //  -put the bluetooth modem into sleep mode?
+      //    command="Q,1", wait for a "AOK" response
+      //  in Q,1 mode, the modem cannot connect OR be discovered
+      bluetooth.println("Q,1");
+      delay(100);// wait for "AOK" response
+    }
+    */
     ioTime = currTime;
-    Serial.print((char)bluetooth.read());
+    Serial.print(nextBtChar);
   }
   if (Serial.available())
   {
