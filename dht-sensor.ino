5623d4a6bf11ad40e39803e030984952e79df6f7
Kyle <ssjmetroid@gmail.com>
2018-07-30 00:38:46 +0000

    Remove a lot of debug code trying to figure out why the stupid bluetooth modem wouldn't go into low-power mode.

--- dht-sensor.ino
+++ dht-sensor.ino
@@ -11,19 +11,6 @@ unsigned int currStringIndexAck = 0;
 DHT dht(PIN_DHT, DHTTYPE);
 SoftwareSerial bluetooth(PIN_BLUETOOTH_TX, PIN_BLUETOOTH_RX);
 unsigned long ioTime = 0;
-// So for WHATEVER bullshit reason, the bluetooth module I got doesn't actually enter
-//  the power-save mode called Sniff Mode unless I keep the stupid fucking thing in
-//  Command-Mode for one full cycle of waiting for the next data packet to send,
-//  so this bool is to let us know if we have passed an ACK->Data cycle entirely
-//  in Command-Mode since power-on.
-int fixedBullshitState;
-// this is to help us determine if we've connected and started the first data cycle
-//  so we can fix the bullshit state (see above)
-bool receivedFirstAck;
-bool isBullshitStateFixed()
-{
-  return fixedBullshitState >= 1;
-}
 // this function is taken from this website:
 //  https://jeelabs.org/2012/05/04/measuring-vcc-via-the-bandgap/
 int vccRead(byte us = 250)
@@ -60,8 +47,6 @@ int serialStringCompare(const char* const str, unsigned strSize,
 }
 void setup()
 {
-  fixedBullshitState = 0;
-  receivedFirstAck   = false;
   // MUST WAIT 500ms for the bluetooth modem to boot
   //  see documentation page 45
   // https://cdn.sparkfun.com/datasheets/Wireless/Bluetooth/bluetooth_cr_UG-v1.0r.pdf
@@ -102,13 +87,6 @@ void setup()
   bluetooth.print("U,9600,N\n");
   delay(100);// wait for "AOK" response
   bluetooth.begin(9600);
-//  // enter command mode on the modem
-//  bluetooth.print("$$$");
-//  delay(100);// wait for "CMD" response
-//  delay(10000);// WHY?...
-//  // leave command mode on the modem
-//  bluetooth.print("---\n");
-//  delay(100);// wait for "END" response
 }
 void loop()
 {
@@ -117,25 +95,6 @@ void loop()
   /// the unsigned long loops after ~50 days or so
   if (currTime - ioTime > 10000)
   {
-//    // enter data mode/reconnect to the bluetooth
-//    //  -Leave quiet mode using "Q,0", wait a little bit for "AOK" response
-//    bluetooth.println("Q,0");
-//    delay(100);// wait for "AOK" response
-
-//    // lower the baud of the modem as well as the serial interface
-//    //  this command also takes the modem out of command mode
-//    bluetooth.println("U,9600,N");
-//    delay(100);// wait for "AOK" response
-//    bluetooth.begin(9600);
-    if(!isBullshitStateFixed() && receivedFirstAck)
-    {
-//      // Exit command mode
-//      bluetooth.print("\n---\n");
-//      delay(100);// wait for "END" response
-      fixedBullshitState++;
-    }
-//    // wait for the modem to reconnect
-//    delay(3000);
     const float humidity    = dht.readHumidity();
     const float temperature = dht.readTemperature();
     const float voltage     = vccRead()/1000.f;
@@ -146,17 +105,13 @@ void loop()
       Serial.println("Corrupted data!");
       return;
     }
-    Serial.println("Humidity= " + String(humidity   , 1) + "%");
-    Serial.println("Temp    = " + String(temperature, 1) + "C");
-    Serial.println("Voltage = " + String(voltage    , 3) + "V");
+//    Serial.println("Humidity= " + String(humidity   , 1) + "%");
+//    Serial.println("Temp    = " + String(temperature, 1) + "C");
+//    Serial.println("Voltage = " + String(voltage    , 3) + "V");
     bluetooth.print(           "H="   +
       String(humidity   , 1) + "%,T=" +
       String(temperature, 1) + "C,V=" +
       String(voltage    , 3) + "U");
-    // wait some time for the data to be transmitted, hopefully
-    //  by the time this delay finishes our bluetooth serial
-    //  connection has an acknowledgement in the buffer.. ~
-    //delay(3000);
     ioTime = currTime;
   }
   if (bluetooth.available())
@@ -166,9 +121,6 @@ void loop()
       ACK_STRING, ACK_STRING_LENGTH, nextBtChar, &currStringIndexAck);
     if(compareToAckStr != 0)
     {
-      Serial.print("[");
-      Serial.print(nextBtChar);
-      Serial.print("]");
       if(compareToAckStr == 2)
       {
         // At this point, we have read the entire ack string
@@ -177,41 +129,19 @@ void loop()
         //  -enter command mode on the bluetooth
         // set the bluetooth serial line to the modem's default baud
         Serial.println("Got Ack!");
+        // Okay so, after much debugging, I have figured out how to force this
+        //  piece of shit bluetooth adapter to enter into a powersave mode
+        //  (SNIFF MODE), by delaying things right here and not transferring any
+        //  data for like 20 seconds once a connection has been established,
+        //  verified with an acknowledgement packet from the server
         delay(15000);
-//        bluetooth.begin(115200);
-//        if(!receivedFirstAck)
-//        {
-//          delay(10000);// WHY?...
-//        }
-        receivedFirstAck = true;
-        if(!isBullshitStateFixed())
-        {
-//          // enter command mode on the modem
-//          bluetooth.print("$$$");
-//          delay(100);// wait for "CMD" response
-          // Okay so, after much debugging, I have figured out how to force this
-          //  piece of shit bluetooth adapter to enter into a powersave mode
-          //  (SNIFF MODE), by delaying things right here and not transferring any
-          //  data for like 20 seconds once a connection has been established,
-          //  verified with an acknowledgement packet from the server
-//          delay(20000);// WHY?...
-        }
-//        //  -put the bluetooth modem into sleep mode?
-//        //    command="Q,1", wait for a "AOK" response
-//        //  in Q,1 mode, the modem cannot connect OR be discovered
-//        bluetooth.println("Q,1");
-//        delay(100);// wait for "AOK" response
-//        //  -kill the bluetooth connection (maybe not needed w/ quiet mode?)
-//        //    command="K,", wait a little bit for the "KILL<cr><lf>" response
-//        bluetooth.println("K,");
-//        delay(100);// wait for "KILL<cr><lf>" response
       }
     }
-    else
-    {
-      ioTime = currTime;
-      Serial.print(nextBtChar);
-    }
+//    else
+//    {
+//      ioTime = currTime;
+//      Serial.print(nextBtChar);
+//    }
   }
 //  if (Serial.available())
 //  {
